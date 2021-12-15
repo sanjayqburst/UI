80a4f4addc06a48f2b8a2714d63cb473d37c1ba2
sanjayqburst <sanjay.mohan@qbusrt.com>
2021-12-15 17:46:13 +0530

    check connectivity status

---
+++ app/src/main/java/com/example/newsapp/network/ConnectivityStatus.kt
@@ -0,0 +1,28 @@
+package com.example.newsapp.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+object ConnectivityStatus {
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork =
+                connectivityManager.getNetworkCapabilities(network) ?: return false
+            when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                else -> false
+            }
+        } else {
+            val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+            networkInfo.isConnected
+        }
+
+
+    }
+}
