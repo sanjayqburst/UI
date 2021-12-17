30114a207648c2b0175fe12b7efb5c107c67cd63
sanjayqburst <sanjay.mohan@qbusrt.com>
2021-12-17 15:56:23 +0530

    Make dataclass parcelable

--- app/src/main/java/com/example/newsapp/model/Article.kt
+++ app/src/main/java/com/example/newsapp/model/Article.kt
@@ -1,12 +1,16 @@
 package com.example.newsapp.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Article(
-    val author: String,
-    val content: String,
-    val description: String,
-    val publishedAt: String,
+    val author: String?,
+    val content: String?,
+    val description: String?,
+    val publishedAt: String?,
     val source: Source,
-    val title: String,
+    val title: String?,
     val url: String,
-    val urlToImage: String
-)
+    val urlToImage: String?
+) : Parcelable
--- app/src/main/java/com/example/newsapp/model/NewsData.kt
+++ app/src/main/java/com/example/newsapp/model/NewsData.kt
@@ -1,7 +1,11 @@
 package com.example.newsapp.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class NewsData(
     val articles: List<Article>,
-    val status: String,
-    val totalResults: Int
-)
+    val status: String?,
+    val totalResults: Int?
+) : Parcelable
