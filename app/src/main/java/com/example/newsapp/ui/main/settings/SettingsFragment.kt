3f60f181fda96177c90c83121e7f1bdaeef48fc6
sanjayqburst <sanjay.mohan@qbusrt.com>
2022-01-07 18:11:45 +0530

    Add profile photo changing with gallery and camera

--- app/src/main/java/com/example/newsapp/ui/main/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/main/settings/SettingsFragment.kt
@@ -3,15 +3,18 @@ package com.example.newsapp.ui.main.settings
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.graphics.Bitmap
-import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
@@ -19,11 +22,15 @@ import com.example.newsapp.R
 import com.example.newsapp.databinding.FragmentSettingsBinding
 import com.example.newsapp.ui.main.MainActivity
 import com.example.newsapp.ui.main.accounts.UserSharedPreference
+import com.example.newsapp.ui.main.homescreen.HomeScreenActivity
 import java.util.*
 
 class SettingsFragment : Fragment() {
     private lateinit var profileBinding: FragmentSettingsBinding
     private lateinit var sharedPreference: UserSharedPreference
+    private lateinit var result: ActivityResultLauncher<Intent>
+    private lateinit var captureImage: Intent
+    private lateinit var selectImage: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +55,27 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        result =
+            this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                Log.d(TAG, "${result.resultCode}")
+                if (result.resultCode == Activity.RESULT_OK) {
+                    if (result.data?.extras?.get("data") is Bitmap) {
+                        val bitMap = result.data?.extras?.get("data") as Bitmap
+                        profileBinding.profileImage.setImageBitmap(bitMap)
+                    } else {
+                        val uri = result.data?.data
+                        profileBinding.profileImage.setImageURI(uri)
+                    }
+
+                } else {
+                    Log.d("Msg", "Error")
+                }
+
+            }
+        selectImage = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        captureImage = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
         val viewPager: ViewPager2 = requireActivity().findViewById(R.id.viewPager)
         profileBinding.apply {
             profileNewsBtnText.setOnClickListener {
@@ -66,28 +94,31 @@ class SettingsFragment : Fragment() {
 
         }
 
-        val result =
-            this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
-                    val bitMap = result.data?.extras?.get("data") as Bitmap
-                    profileBinding.profileImage.setImageBitmap(bitMap)
-                } else {
-                    Log.d("Msg", "Error")
-                }
-            }
-        val captureImage = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
-        profileBinding.fabButton.setOnClickListener {
+
+
+
+        profileBinding.favButton.setOnClickListener {
+            Log.d(TAG, "Clicked fav button")
             if (ActivityCompat.checkSelfPermission(
                     requireContext(),
                     android.Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED
             ) {
-                result.launch(captureImage)
+                Log.d(TAG, "Permissions granted")
+                showAlert()
+//                result.launch(captureImage)
             } else {
+                Log.d(TAG, "Permission Denied")
                 ActivityCompat.requestPermissions(
                     requireActivity(),
-                    arrayOf(android.Manifest.permission.CAMERA),
+                    arrayOf(
+                        android.Manifest.permission.CAMERA,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    ),
                     100
                 )
             }
@@ -95,15 +126,45 @@ class SettingsFragment : Fragment() {
     }
 
     private fun localeChange(localeCode: String) {
-        val config = resources.configuration
+
+
+        val configuration = Configuration()
         val locale = Locale(localeCode)
         Locale.setDefault(locale)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            config.setLocale(locale)
-        } else {
-            config.locale = locale
+        configuration.setLocale(locale)
+        requireActivity().baseContext.resources.updateConfiguration(
+            configuration,
+            requireActivity().baseContext.resources.displayMetrics
+        )
+        val refresh = Intent(requireActivity(), HomeScreenActivity::class.java)
+        startActivity(refresh)
+
+    }
+
+    companion object {
+        private const val TAG = "News App"
+    }
+
+    private fun showAlert() {
+
+        val profilePictureAlertView = LayoutInflater.from(requireContext())
+            .inflate(R.layout.alert_dialogue_profile_image_change, null)
+        val profilePictureAlertBuilder =
+            AlertDialog.Builder(requireContext()).setView(profilePictureAlertView).create()
+        profilePictureAlertBuilder.show()
+        val camera = profilePictureAlertView.findViewById<ImageButton>(R.id.profilePhotoCamera)
+        camera.setOnClickListener {
+
+            result.launch(captureImage)
+            profilePictureAlertBuilder.dismiss()
         }
-        resources.updateConfiguration(config, resources.displayMetrics)
+        val gallery = profilePictureAlertView.findViewById<ImageButton>(R.id.profilePhotoGallery)
+        gallery.setOnClickListener {
+            result.launch(selectImage)
+            profilePictureAlertBuilder.dismiss()
+
+        }
+
     }
 
 
