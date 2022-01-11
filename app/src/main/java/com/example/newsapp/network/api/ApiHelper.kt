38cf3129b0677d45f1ea75aa5590e6ecc479c0ff
sanjayqburst <sanjay.mohan@qbusrt.com>
2022-01-11 17:21:51 +0530

    Change inline function to functional body

--- app/src/main/java/com/example/newsapp/network/api/ApiHelper.kt
+++ app/src/main/java/com/example/newsapp/network/api/ApiHelper.kt
@@ -1,5 +1,9 @@
 package com.example.newsapp.network.api
 
+import com.example.newsapp.model.NewsData
+
 class ApiHelper(private val apiService: ApiService) {
-    suspend fun getNewsData() = apiService.newsApiData()
+    suspend fun getNewsData(): NewsData {
+        return apiService.newsApiData()
+    }
 }
