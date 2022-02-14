72ad3a454a013849513dfd2b2057d1c7a632268f
qburst <sanjay.mohan@qburst.com>
2022-02-14 12:07:44 +0530

    Add new function to get news favourites data

--- app/src/main/java/com/example/newsapp/network/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/network/repository/NewsRepository.kt
@@ -1,11 +1,50 @@
 package com.example.newsapp.network.repository
 
-import com.example.newsapp.model.NewsData
+import androidx.lifecycle.liveData
+import com.example.newsapp.db.NewsFavouritesDao
+import com.example.newsapp.model.Article
 import com.example.newsapp.network.api.ApiHelper
+import com.example.newsapp.utils.Resource
+import kotlinx.coroutines.Dispatchers
 
 // Instance of API helper
-class NewsRepository(private val apiHelper: ApiHelper) {
-    suspend fun getNewsData(): NewsData {
-        return apiHelper.getNewsData()
+
+class NewsRepository(
+    private val apiHelper: ApiHelper,
+    private val newsFavouritesDao: NewsFavouritesDao
+) {
+
+    fun getNewsData() = liveData(Dispatchers.IO) {
+        emit(Resource.loading(data = null))
+        try {
+            emit(Resource.success(data = apiHelper.getNewsData()))
+        } catch (ex: Exception) {
+            emit(Resource.error(data = null, message = "Error ${ex.localizedMessage}"))
+        }
     }
+
+    fun getFavData(uid: String) = liveData(Dispatchers.IO) {
+        emit(Resource.loading(data = null))
+        try {
+            val articleArray = arrayListOf<Article>()
+            val data = apiHelper.getNewsData()
+            val favData = newsFavouritesDao.getFavList(uid)
+            val array = arrayListOf<String>()
+            for (item in favData) {
+                array.add(item.favourLink)
+            }
+            if (data != null) {
+                for (item in data.articles) {
+                    if (item.url in array) {
+                        articleArray.add(item)
+                    }
+                }
+            }
+            emit(Resource.success(data = articleArray))
+
+        } catch (e: Exception) {
+            emit(Resource.error(data = null, message = "Error ${e.localizedMessage}"))
+        }
+    }
+
 }
--- app/src/main/java/com/example/newsapp/ui/main/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/main/viewmodel/NewsViewModel.kt
@@ -1,19 +1,19 @@
 package com.example.newsapp.ui.main.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
+import com.example.newsapp.model.Article
+import com.example.newsapp.model.NewsData
 import com.example.newsapp.network.repository.NewsRepository
 import com.example.newsapp.utils.Resource
-import kotlinx.coroutines.Dispatchers
 
-class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
-    //
-    fun getNewsData() = liveData(Dispatchers.IO) {
-        emit(Resource.loading(data = null))
-        try {
-            emit(Resource.success(data = newsRepository.getNewsData()))
-        } catch (exception: Exception) {
-            emit(Resource.error(data = null, message = exception.message ?: "Error occurred"))
-        }
+
+class NewsViewModel(private val newsRepository: NewsRepository):ViewModel(){
+    fun getNewsData(): LiveData<Resource<NewsData?>> {
+        return newsRepository.getNewsData()
     }
+    fun getFavData(uId:String): LiveData<Resource<ArrayList<Article>>> {
+        return newsRepository.getFavData(uId)
+    }
+
 }
